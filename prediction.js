8d2db0f9827b5b09a76e903c4d3f8ec73a1a2c31
Dillon Hunt <90228106+Dillon-Hunt@users.noreply.github.com>
2022-10-13 17:00:05 +1000

    Added comments and minor changes.

--- prediction.js
+++ prediction.js
@@ -4,23 +4,24 @@
 //    as a part of specialist mathematics IA1.
 //
 
+// Import libraries
 const { multiply, add, number } = require('mathjs')
 const { readFile } = require('fs')
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+const readline = require('readline').createInterface({ input: process.stdin, output: process.stdout });
 
-const teams = ['PE', 'CR', 'NQC', 'NK', 'SSR', 'PP', 'MWSE', 'SGID', 'CBB', 'GCT', 'BB', 'NZW', 'CSS', 'SR', 'MS', 'WT']
+// Initialize teams variable
+let teams = [] // Teams: 'PE', 'CR', 'NQC', 'NK', 'SSR', 'PP', 'MWSE', 'SGID', 'CBB', 'GCT', 'BB', 'NZW', 'CSS', 'SR', 'MS', 'WT'
 
+// Get's dominance vector from dominance matrix by adding up each row.
 function getVector(dominance_matrix) {
     return dominance_matrix.map(row => {
         return row.reduce((a, b) => number(a) + number(b))
     })
 }
 
-function pow(matrix, order) {
 
+// Calculates a matrix to the nth power
+function pow(matrix, order) {
     let new_matrix = matrix
 
     for (let i = 1; i < order; i++) {
@@ -30,25 +31,41 @@ function pow(matrix, order) {
     return new_matrix
 }
 
+// Gets input for two teams and prints the expected result
 function askTeams(results) {
-    readline.question('Enter Teams: ', teams => {
+    readline.question('\nEnter Teams: ', teams => {
+
+        // End loop
         if (teams === '') return
+
+        // Print result
         else {
-            teams = teams.split(' vs ')
 
+            // Split 'PP-NQC' into ['PP', 'NQC']
+            teams = teams.split('-')
+
+            // Get winner
             if (results[teams[0]] > results[teams[1]]) console.log(`Team ${teams[0]} should win.\n`)
-            else console.log(`Team ${teams[1]} should win.\n`)
+            else console.log(`Team ${teams[1]} should win.`)
 
+            // Continue Loop
             askTeams(results)
         }
     })
 }
 
 async function calculate() {
+
+    // Create matrix array from csv data stored in './scores.csv'
     let game_results_matrix = await new Promise((resolve) => 
         {
             readFile('./scores.csv', (err, data) => {
-                rows = data.toString().split('\n')
+                csv = data.toString().split('\n')
+
+                teams_raw = csv.shift()
+                teams = teams_raw.split(',')
+
+                rows = csv
         
                 rows.forEach((row, id) => {
                     rows[id] = row.split(',')
@@ -60,38 +77,44 @@ async function calculate() {
         }
     )
     
-
+    // Initialize list of vector orders
     let vector_orders = []
 
+    // Append each order up to the number of teams
     for (i = 0; i < game_results_matrix.length - 1; i++) {
         vector_orders.push(getVector(pow(game_results_matrix, i + 1)))
     }
 
+    // Initialize resultant vector
     let resultant_vector = multiply(vector_orders[0], vector_orders.length)
 
+    // While the resultant vector is not distinct, add next order multiplied by 16 - n
     {
         let i = 0
 
         while (resultant_vector.length !== new Set(resultant_vector).size) {
             i++
-
             resultant_vector = add(resultant_vector, multiply(vector_orders[i], vector_orders.length - i))
         }
     }
 
+    // Initialize results array
     let results = []
 
+    // Add teams and scores to results array
     resultant_vector.forEach((team, id) => {
         results.push([team, teams[id]])
     })
 
+    // Initialize results object (for comparison)
     let results_object = {}
 
-    // results.sort((a, b) => b[0] - a[0]).forEach(team => console.log(team[1]))
+    // Sort results then print and construct results object
+    const sorted_results = results.sort((a, b) => b[0] - a[0])
+    sorted_results.forEach(team => results_object[team[1]] = team[0])
+    sorted_results.forEach((team, index) => console.log(`${index + 1}: ${(index + 1).toString().length > 1 ? '' : ' '}${team[1]}`))
 
-    results.sort((a, b) => b[0] - a[0]).forEach(team => results_object[team[1]] = team[0])
-    
-
+    // Ask for two teams to compare
     askTeams(results_object)
 }
 
